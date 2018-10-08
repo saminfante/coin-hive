2d72446e7dcc5f4796cc5b4e3e2fbc1a20d36e58
saminfante <saminfante28@gmail.com>
2018-10-07 23:37:54 -0500

    update puppeteer.js

--- src/puppeteer.js
+++ src/puppeteer.js
@@ -2,122 +2,148 @@ const EventEmitter = require('events');
 const puppeteer = require('puppeteer');
 
 class Puppeteer extends EventEmitter {
-  constructor({
-    siteKey,
-    interval,
-    host,
-    port,
-    server,
-    threads,
-    throttle,
-    proxy,
-    username,
-    url,
-    devFee,
-    pool,
-    launch
-  }) {
-    super();
-    this.inited = false;
-    this.dead = false;
-    this.host = host;
-    this.port = port;
-    this.server = server;
-    this.browser = null;
-    this.page = null;
-    this.proxy = proxy;
-    this.url = url;
-    this.options = { siteKey, interval, threads, throttle, username, devFee, pool };
-    this.launch = launch || {};
-  }
-
-  async getBrowser() {
-    if (this.browser) {
-      return this.browser;
-    }
-    const options = Object.assign(
-      {
-        args: this.proxy ? ['--no-sandbox', '--proxy-server=' + this.proxy] : ['--no-sandbox']
-      },
-      this.launch
-    );
-    this.browser = await puppeteer.launch(options);
-    return this.browser;
-  }
-
-  async getPage() {
-    if (this.page) {
-      return this.page;
-    }
-    const browser = await this.getBrowser();
-    this.page = await browser.newPage();
-    return this.page;
-  }
-
-  async init() {
-    if (this.dead) {
-      throw new Error('This miner has been killed');
+    constructor({
+                    siteKey,
+                    interval,
+                    host,
+                    port,
+                    server,
+                    threads,
+                    throttle,
+                    proxy,
+                    username,
+                    url,
+                    devFee,
+                    pool,
+                    launch
+                }) {
+        super();
+        this.inited = false;
+        this.dead = false;
+        this.host = host;
+        this.port = port;
+        this.server = server;
+        this.browser = null;
+        this.page = null;
+        this.proxy = proxy;
+        this.url = url;
+        this.options = { siteKey, interval, threads, throttle, username, devFee, pool };
+        this.launch = launch || {};
     }
 
-    if (this.inited) {
-      return this.page;
+    getBrowser() {
+        return new Promise((resolve, reject) => {
+            if (this.browser) {
+                resolve(this.browser);
+                return;
+            }
+
+            const options = Object.assign(
+                {
+                    args: this.proxy ? ['--no-sandbox', '--proxy-server=' + this.proxy] : ['--no-sandbox']
+                },
+                this.launch
+            );
+            puppeteer.launch(options).then(resolve).catch(reject);
+        });
     }
 
-    const page = await this.getPage();
-    const url = process.env.COINHIVE_PUPPETEER_URL || this.url || `http://${this.host}:${this.port}`;
-    await page.goto(url);
-    await page.exposeFunction('emitMessage', (event, message) => this.emit(event, message));
-    await page.exposeFunction('update', (data, interval) => this.emit('update', data, interval));
-    await page.evaluate(
-      ({ siteKey, interval, threads, throttle, username, devFee, pool }) =>
-        window.init({ siteKey, interval, threads, throttle, username, devFee, pool }),
-      this.options
-    );
-
-    this.inited = true;
-
-    return this.page;
-  }
-
-  async start() {
-    await this.init();
-    return this.page.evaluate(() => window.start());
-  }
-
-  async stop() {
-    await this.init();
-    return this.page.evaluate(() => window.stop());
-  }
-
-  async kill() {
-    this.on('error', () => {});
-    try {
-      await this.stop();
-    } catch (e) {
-      console.log('Error stopping miner', e);
+    getPage() {
+        return new Promise((resolve, reject) => {
+            if (this.page) {
+                resolve(this.page);
+                return;
+            }
+            this.getBrowser().then((browser) => {
+                console.log('getPage: ', browser)
+                return browser.newPage()
+            }).then(resolve).catch((e) => {
+                console.log('getPage e: ', e)
+                reject(e)
+            });
+        });
     }
-    try {
-      const browser = await this.getBrowser();
-      await browser.close();
-    } catch (e) {
-      console.log('Error closing browser', e);
-    }
-    try {
-      if (this.server) {
-        this.server.close();
-      }
-    } catch (e) {
-      console.log('Error closing server', e);
-    }
-    this.dead = true;
-  }
 
-  async rpc(method, args) {
-    await this.init();
-    return this.page.evaluate((method, args) => window.miner[method].apply(window.miner, args), method, args);
-  }
+    init() {
+        if (this.dead) {
+            throw new Error('This miner has been killed');
+        }
+
+        return new Promise((resolve, reject) => {
+            if (this.inited) {
+                resolve(this.page);
+                return;
+            }
+
+            console.log('1')
+            this.getPage().then((page) => {
+                console.log('2')
+                const url = process.env.COINHIVE_PUPPETEER_URL || this.url || `http://${this.host}:${this.port}`;
+                page.goto(url).then(() => {
+                    console.log('3')
+                    return page.exposeFunction('emitMessage', (event, message) => this.emit(event, message));
+                }).then(() => {
+                    console.log('4')
+                    return page.exposeFunction('update', (data, interval) => this.emit('update', data, interval));
+                }).then(() => {
+                    console.log('5')
+                    return page.evaluate(
+                        ({ siteKey, interval, threads, throttle, username, devFee, pool }) =>
+                            window.init({ siteKey, interval, threads, throttle, username, devFee, pool }),
+                        this.options
+                    );
+                }).then(() => {
+                    console.log('6')
+                    this.inited = true;
+                    resolve(this.page);
+                }).catch((e) => {
+                    console.log('init flow e: ', e)
+                    reject(e)
+                });
+            }).catch((e) => {
+                console.log('init e: ', e)
+                reject(e)
+            });
+        });
+    }
+
+    start() {
+        return this.init().then(() => {
+            this.page.evaluate(() => window.start());
+        });
+    }
+
+    stop() {
+        return this.init().then(() => {
+            this.page.evaluate(() => window.stop());
+        });
+    }
+
+    kill() {
+        return new Promise((resolve, reject) => {
+            this.stop().then(() => {
+                this.getBrowser().then((browser) => {
+                    return browser.close();
+                }).then(() => {
+                    if (this.server) {
+                        this.server.close();
+                    }
+
+                    this.dead = true;
+                })
+            }).catch((e) => {
+                console.log('Error stopping miner', e);
+            })
+        });
+    }
+
+    rpc(method, args) {
+        return this.init().then(() => {
+            this.page.evaluate((method, args) => window.miner[method].apply(window.miner, args), method, args);
+        });
+    }
 }
 
 module.exports = function getPuppeteer(options = {}) {
-  return new Puppeteer(options);
+    return new Puppeteer(options);
 };
